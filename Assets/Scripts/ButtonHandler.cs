3e4243bde6e05decf3301d9d892377bc59a2f279
robofriven <robofriven@yahoo.com>
2015-09-11 05:47:50 -0700

    Got Multiplayer to send/receive cards
    send/receive working, but not doing outcome for both clients.

--- Assets/Scripts/ButtonHandler.cs
+++ Assets/Scripts/ButtonHandler.cs
@@ -7,23 +7,30 @@ public class ButtonHandler : MonoBehaviour
     public GameController gameController;
     public Console console;
     public MultiController multiController;
+    public PhotonView photonView;
    
 
     void Start()
     {
         gameController = GetComponent<GameController>();
         console = GameObject.FindObjectOfType<Console>() as Console;
+        multiController = GetComponent<MultiController>();
     }
 
+    // Button that says both players are ready
     public void GoButton()
     {
-        gameController.Play();
+        //gameController.Play();
+        multiController.CardsReady();
+
+
     }
 
     internal void StartGame()
     {
         string message = "Play your cards and press the go button";
         console.Display(message);
+        console.ClearButtons();
         gameController.StartRound();
     }
 
@@ -31,6 +38,7 @@ public class ButtonHandler : MonoBehaviour
     {
         Debug.Log("The game would start");
         console.Display("The game should begin shortly...");
-        multiController.Ready();        
+        multiController.Ready();
+        console.ClearButtons();   
     }
 }
--- Assets/Scripts/MultiController.cs
+++ Assets/Scripts/MultiController.cs
@@ -1,11 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public class MultiController : MonoBehaviour {
+public class MultiController : MonoBehaviour
+{
 
     // Public variables and fields
     public int ready = 0;
-    public bool PlayersReady = false;
 
     [Header("Player info")]
     public int startingHealth = 20;
@@ -16,18 +16,32 @@ public class MultiController : MonoBehaviour {
     public int oppHealth;
     public int myScore;
     public int OppScore;
+    [Header("Card Values")]
+    public int myAttack;
+    public int oppAttack;
+    public int myDefense;
+    public int oppDefense;
     [Space(10)]
     public int numCardTypes = 17;
     [Header("Fields")]
     public Console console;
+    public GameObject attackField;
+    public GameObject defenseField;
     [Header("Prefabs")]
     public Card cardPrefab;
 
     // Private Variables
     private Hand hand;
     private PhotonView photonView;
+    private bool PlayersReady           =     false;
+    private bool RoundReady             =     false;
+    private bool cardsReady             =     false;
+    private bool waitForClick           =     false;
+    private bool cardsSent              =     false;
+    private bool cardsReceived          =     false;
+    private int roundReady              =       0;
 
-    
+
     void Start()
     {
         photonView = GetComponent<PhotonView>();
@@ -45,18 +59,56 @@ public class MultiController : MonoBehaviour {
     {
         ready++;
     }
-        
+
 
 
     void Update()
     {
+        //Debug.Log(PlayersReady);
         if (!PlayersReady)
         {
             if (ready == 2)
             {
                 PlayersReady = true;
-                photonView.RPC("StartGame", PhotonTargets.All);                
+                if (PhotonNetwork.isMasterClient)
+                {
+                    photonView.RPC("StartGame", PhotonTargets.All);
+                }
             }
+        }
+
+        if (!RoundReady)
+        {
+            if (roundReady == 2)
+            {
+                RoundReady = true;
+                if (PhotonNetwork.isMasterClient)
+                {
+                    photonView.RPC("Play", PhotonTargets.All);
+                }
+                
+            }
+        }
+
+        if (waitForClick)
+        {
+            if (Input.GetButton("Fire1"))
+            {
+                waitForClick = false;
+                NextRound();
+            }
+        }
+
+        //Debug.Log(cardsSent);
+        //Debug.Log(cardsReceived);
+        //if (cardsReceived)
+        //{
+        //    Outcome();
+        //}
+
+        if (myAttack != 0 && myDefense != 0 && oppAttack != 0 && oppDefense != 0)
+        {
+            Outcome();
         }
     }
 
@@ -75,4 +127,88 @@ public class MultiController : MonoBehaviour {
         oppHealth = startingHealth;
         hand = new Hand(DeckSize, handSize, numCardTypes, cardPrefab);
     }
+
+
+    public void CardsReady()
+    {
+        photonView.RPC("RoundIntermediate", PhotonTargets.All);
+        //Debug.Log("Player is ready!!!");
+        console.Display("This is not firing correctly");
+        console.ClearButtons();
+    }
+
+    [PunRPC]
+    public void RoundIntermediate()
+    {
+        roundReady++;
+    }
+
+
+    [PunRPC]
+    public void Play()
+    {
+        Debug.Log("Play is called");
+        console.Display("Play logic will soon commence");
+
+        myAttack = attackField.GetComponentInChildren<Card>().attack;
+        myDefense = defenseField.GetComponentInChildren<Card>().defense;
+        cardsReady = true;
+       
+
+    }
+
+    private void Outcome()
+    {
+        int myDamage = myAttack - oppDefense;
+        int oppDamage = oppAttack - myDefense;
+
+        if (myDamage < 0)
+            myDamage = 0;
+        if (oppDamage < 0)
+            oppDamage = 0;
+
+        myHealth -= oppDamage;
+        oppHealth -= myDamage;
+
+
+        string message = string.Format("You did {0} damage. \nYour opponent did {1} damage. \nYour health is {2}, while his health is {3}. \nClick to continue.", myDamage, oppDamage, myHealth, oppHealth);
+        console.Display(message);
+        console.ClearButtons();
+        
+
+        waitForClick = true;
+    }
+
+    private void NextRound()
+    {
+        Debug.Log("NextRound Called");
+        round++;
+        if (round > 2)
+        {
+            // Do whatever it takes to end the game
+            // Popup to play another
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (cardsReady)
+        {
+            if (stream.isWriting)
+            {
+                //stream.SendNext(thing);
+                stream.SendNext(myAttack);
+                stream.SendNext(myDefense);
+
+            }
+            if (stream.isReading)
+            {
+                //thing = stream.ReceiveNext();
+                oppAttack = (int)stream.ReceiveNext();
+                oppDefense = (int)stream.ReceiveNext();
+                cardsReceived = true;
+            }
+        }
+    }
+
 }
